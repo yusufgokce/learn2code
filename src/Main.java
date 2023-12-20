8ece871d611703583fde5896a149243f9ebe49f0
yu <yusuf.gokce@mail.utoronto.ca>
2023-12-20 03:03:45 -0500

    Creating blueprint for the game - this program takes a string that is in a runnable .java class format, compiles it and then runs it.

---
+++ src/Main.java
@@ -0,0 +1,65 @@
+import javax.tools.*;
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.List;
+import java.net.URI;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String code = "public class HelloWorld {" +
+                "   public static void main(String[] args) {" +
+                "       System.out.println(\"Hello, World!\");" +
+                "   }" +
+                "}";
+
+        // Create a compilation task
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+        JavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+
+        // Create a source file
+        JavaFileObject compilationUnit = new DynamicJavaSourceCodeObject("HelloWorld", code);
+
+        Iterable<? extends JavaFileObject> compilationUnits = List.of(compilationUnit);
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
+
+        // Perform the compilation
+        boolean success = task.call();
+
+        if (success) {
+            // Run the compiled class
+            try {
+                URLClassLoader classLoader = new URLClassLoader(new URL[]{new File(".").toURI().toURL()});
+                Class<?> loadedClass = classLoader.loadClass("HelloWorld");
+                loadedClass.getDeclaredMethod("main", String[].class).invoke(null, (Object) new String[]{});
+            } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | MalformedURLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // Display compilation errors
+            for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) {
+                System.out.format("Error on line %d in %s%n",
+                        diagnostic.getLineNumber(),
+                        diagnostic.getSource().toUri());
+            }
+        }
+    }
+}
+
+class DynamicJavaSourceCodeObject extends SimpleJavaFileObject {
+    private final String code;
+
+    DynamicJavaSourceCodeObject(String name, String code) {
+        super(URI.create("string:///" + name.replace('.', '/') + Kind.SOURCE.extension), Kind.SOURCE);
+        this.code = code;
+    }
+
+    @Override
+    public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+        return code;
+    }
+}
